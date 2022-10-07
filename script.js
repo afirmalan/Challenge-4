0d42b6be2b1cd88d5d8283a8b728a7818c1f1ab2
Adrian Firmalan <afirmalan@yahoo.com>
2022-10-07 11:31:56 -0700

    base html and base js

---
+++ script.js
@@ -0,0 +1,46 @@
+// variables (questions array)
+var timerElement = document.querySelector(".timer");
+var Prompts = document.querySelector(".prompts");
+var startButton = document.querySelector(".startButton");
+
+var score = "";
+
+prompt = [""]
+// function on counter start with base count (6000)
+function onStart() {
+    timerCount = 30,
+    // prompt question from api
+    renderQuestions(),
+    startTimer()
+
+}
+
+
+
+
+function renderQuestions(){
+    // on true function, prompt question
+    // loop function to loop through prompts if count > 0
+    for (let i = 0; i > prompts.length; i++) {
+
+
+    }
+
+
+    // on false answer, take time away from overall time
+    // loop function to loop through prompts if count > 0
+
+
+
+}
+
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount --;
+        timerElement.textcontent = timerCount;
+    })
+}
+
+function init() {
+    getScore()
+}
