a33612430da2d9e7043f17b76252a48f33d0606a
Adrian Firmalan <afirmalan@yahoo.com>
2022-10-16 15:25:20 -0400

    render questions

--- script.js
+++ script.js
@@ -4,14 +4,32 @@ var timeEl = document.querySelector(".time");
 var introEl = document.querySelector("intro")
 var questionsEl = document.querySelector(".questions");
 var startButtonEl = document.querySelector(".startButton");
-var init = document.querySelector(".input-init")
-var dashboard = document.querySelector(".dashboard")
-
-
+var init = document.querySelector(".input-init");
+var dashboard = document.querySelector(".dashboard");
+var clockid = document.querySelector(".timer");
+var answer1El = document.querySelector("#answer1");
+var answer2El = document.querySelector("#answer2");
+var answer3El = document.querySelector("#answer3");
+var answer4El = document.querySelector("#answer4");
 var score = "";
 
-prompt = [""]
-// function on counter start with base count (6000)
+var questions = [{
+    title: "new question 1",
+    answers: ["answer1, answer2, answer 3, answer 4"],
+    solution: "answer2"
+}, {
+    title: "new question 2",
+    answers: ["answer1, answer2, answer 3, answer 4"],
+    solution: "answer1"
+}, {
+    title: "new question 3",
+    answers: ["answer1, answer2, answer 3, answer 4"],
+    solution: "answer3"
+}];
+
+var index = 0
+
+// function on counter start with base count (7500)
 function onStart() {
     timerCount = 75;
     questionsEl.classList.remove(".hide");
@@ -21,18 +39,18 @@ function onStart() {
     renderQuestions();
     startTimer();
 
-}
-
+};
 
 
 
 function renderQuestions() {
     // on true function, prompt question
     // loop function to loop through prompts if count > 0
-    for (let i = 0; i > prompts.length; i++) {
-        textContent
-
-    }
+    titleEl.textContent = questions[index].title
+    answer1El.textContent=questions[index].answers[0]
+    answer2El.textContent=questions[index].answers[1]
+    answer3El.textContent=questions[index].answers[2]
+    answer4El.textContent=questions[index].answers[3]
 
 
     // on false answer, take 15 sec away from overall time
@@ -40,14 +58,15 @@ function renderQuestions() {
 
 
 
-}
+};
 
 function startTimer() {
     timer = setInterval(function () {
-        timerCount--;
         timeEl.textContent = timerCount;
+        timerCount--;
+
     })
-}
+};
 
 
 // presented with the score and input for initials. Timer should stop and the time left is the score
